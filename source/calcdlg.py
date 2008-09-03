f852e9481ae2d307a0c38cc4d0e6199ec4390f33
Doug Bell <doug101@bellz.org>
2008-09-03 09:23:46 -0400

    removed Alt-R shortcut

--- source/calcdlg.py
+++ source/calcdlg.py
@@ -457,10 +457,6 @@ class CalcDlg(QtGui.QWidget):
             return
         letter = str(keyEvent.text()).upper()
         if keyEvent.modifiers() == QtCore.Qt.AltModifier:
-            if letter == 'R':
-                button = self.cmdDict.get('CLR')
-                button.clickEvent()
-                button.tmpDown(300)
             if self.altBaseView and self.altBaseView.isVisible():
                 if letter in ('X', 'O', 'B', 'D'):
                     self.altBaseView.setCodedBase(letter, False)
