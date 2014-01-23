e9373918d57ce3423479350cc5e6557ce2218334
Doug Bell <doug101@bellz.org>
2014-01-22 21:04:06 -0500

    update linux & win installers

--- source/setup.py
+++ source/setup.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
-from distutils.core import setup
-import py2exe
+
+#****************************************************************************
+# setup.py, provides a distutils script for use with cx_Freeze
+#
+# Creates a standalone windows executable
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+#
+# rpCalc, an RPN calculator
+# Copyright (C) 2014, Douglas W. Bell
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License, either Version 2 or any later
+# version.  This program is distributed in the hope that it will be useful,
+# but WITTHOUT ANY WARRANTY.  See the included LICENSE file for details.
+#*****************************************************************************
+
+import sys
+from cx_Freeze import setup, Executable
+from rpcalc import __version__
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+extraFiles =  [('../doc', 'doc'), ('../icons', 'icons'),
+               ('../source', 'source'), ('../win', '.')]
 
 setup(name = 'rpcalc',
-      windows = [{'script': 'rpcalc.py',
-                  'icon_resources': [(1, '../win/rpcalc.ico')]}],
-      options = {'py2exe': {'includes': ['sip'],
-                            'dist_dir': 'dist/lib'}})
-
-# run with: python setup.py py2exe
+      version = __version__,
+      description = 'rpCalc, an RPN calculator',
+      options = {'build_exe': {'includes': 'atexit',
+                               'include_files': extraFiles,
+                               'excludes': ['*.pyc'],
+                               'icon': '../win/rpcalc.ico',
+                               'build_exe': '../../rpCalc-0.7'}},
+      executables = [Executable('rpcalc.py', base=base)])
---
+++ win/rpcalc-user.iss
@@ -0,0 +1,44 @@
+; rpcalc-user.iss
+
+; Inno Setup installer script for rpCalc, an RPN calculator
+; This will install for a single user, no admin rights are required.
+
+[Setup]
+AppName=rpCalc
+AppVersion=0.7.0
+DefaultDirName={userappdata}\rpCalc
+DefaultGroupName=rpCalc
+DisableProgramGroupPage=yes
+OutputDir=.
+OutputBaseFilename=rpcalc-0.7.0-install-user
+PrivilegesRequired=lowest
+SetupIconFile=rpcalc.ico
+Uninstallable=IsTaskSelected('adduninstall')
+UninstallDisplayIcon={app}\rpcalc.exe,0
+
+[Tasks]
+Name: "startmenu"; Description: "Add start menu shortcuts"
+Name: "deskicon"; Description: "Add a desktop shortcut"
+Name: "adduninstall"; Description: "Create an uninstaller"
+Name: "source"; Description: "Include source code"
+
+[Files]
+Source: "rpcalc.exe"; DestDir: "{app}"
+Source: "library.zip"; DestDir: "{app}"
+Source: "*.dll"; DestDir: "{app}"
+Source: "*.pyd"; DestDir: "{app}"
+Source: "doc\*.html"; DestDir: "{app}\doc"
+Source: "doc\LICENSE"; DestDir: "{app}\doc"
+Source: "icons\*.png"; DestDir: "{app}\icons"
+Source: "source\*.py"; DestDir: "{app}\source"; Tasks: "source"
+Source: "rpcalc.ico"; DestDir: "{app}"; Tasks: "source"
+Source: "*.iss"; DestDir: "{app}"; Tasks: "source"
+
+[Icons]
+Name: "{userstartmenu}\rpCalc"; Filename: "{app}\rpcalc.exe"; \
+      WorkingDir: "{app}"; Tasks: "startmenu"
+Name: "{group}\rpCalc"; Filename: "{app}\rpcalc.exe"; \
+      WorkingDir: "{app}"; Tasks: "startmenu"
+Name: "{group}\Uninstall"; Filename: "{uninstallexe}"; Tasks: "startmenu"
+Name: "{userdesktop}\rpCalc"; Filename: "{app}\rpcalc.exe"; \
+      WorkingDir: "{app}"; Tasks: "deskicon"
